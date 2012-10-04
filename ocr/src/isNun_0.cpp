b6fc8e421e62952ded31485d870710de2de1fc3e
dengzhe <zhedeng@hustunique.com>
2012-10-04 14:44:44 +0000

    dengzhe

--- ocr/src/isNun_0.cpp
+++ ocr/src/isNun_0.cpp
@@ -7,16 +7,21 @@ int isNun_0(unsigned char *res) {
 	int judge = 0;
 
 	//实现部分，返回1表示识别出，0表示识别不出
+	unsigned char *bg;
+	bg=(unsigned char*)malloc(sizeof(unsigned char)*widthOfImage*heightOfImage);
+	memcpy(*bg,*res,sizeof(unsigned char)*widthOfImage*heightOfImage);
+	
 	float A_thr=0.95,B_thr=0.6,L_thr=0.5;//判断0的三个特征值
 	int num=Num_Cir(*res);
+	int i,j;
 	if(num==1)
 	{
 		int A=0; 
 	    int B=0;
-		for(int i=0;i<heightOfImage;i++)
+		for(i=0;i<heightOfImage;i++)
 		{
 			int temp=0;
-			for(int j=1;j<=widthOfImage;j++)
+			for(j=1;j<=widthOfImage;j++)
 			{
               res[i*widthOfImage+j]==1 ? temp++:temp=temp;
 			}
@@ -27,6 +32,26 @@ int isNun_0(unsigned char *res) {
 			else if(temp !=0)
 			{
 				B++;
+			}
+		}
+		for(i=1;i<heightOfImage-1;i++)
+		{
+			for(j=2;j<heightOfImage;j++)
+			{
+				if(res[i*widthOfImage+j]==1)
+				{
+				if(bg[(i-1)*widthOfImage+j-1]==2
+				   ||(bg[(i-1)*widthOfImage+j]==2
+				   ||(bg[(i-1)*widthOfImage+j+1]==2
+				   ||(bg[(i)*widthOfImage+j-1]==2
+				   ||(bg[(i)*widthOfImage+j+1]==2
+				   ||(bg[(i+1)*widthOfImage+j-1]==2
+				   ||(bg[(i+1)*widthOfImage+j]==2
+				   ||(bg[(i+1)*widthOfImage+j+1]==2)
+				   {
+				    
+				   }
+				 }
 			}
 		}
 		A/B>=A_thr?judue =1:judue=0;
