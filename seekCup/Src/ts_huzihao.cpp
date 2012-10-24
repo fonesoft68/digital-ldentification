feeb26f4e2d238056ab6c21b8da55effc91e452f
huzihao <zihaohu@hustunique>
2012-10-24 14:50:29 +0800

    aaaa

---
+++ seekCup/Src/ts_huzihao.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sql.h"
+
+database *nowUsedDatabase;
+database *allDatabaseRoot;
+
+int main(int argc, char *argv[])
+{
+  database * allDatabaseRoot = createDatabase("huzi");
+}
