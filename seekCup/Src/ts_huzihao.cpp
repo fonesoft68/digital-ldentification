fd35d27ca31073755c0c8748c0906ff138dbb3f3
huzihao <zihaohu@hustunique>
2012-10-25 21:52:06 +0800

    alter

--- seekCup/Src/ts_huzihao.cpp
+++ seekCup/Src/ts_huzihao.cpp
@@ -9,26 +9,23 @@ int databaseCnt = 0;
 int main(int argc, char *argv[])
 {
   
-  allDatabaseRoot->next = NULL;
-  if(!allDatabaseRoot) {
-    printf("all is empty\n");
-  }
-  createDatabase("huzi");
-  createDatabase("hao");
-  createDatabase("nimei");
-  allDatabaseRoot->next->rootTable = createTable("  a int  , b float  , noe  ");
-  allDatabaseRoot->next->rootTable->name = "a";
-  allDatabaseRoot->next->rootTable->next = createTable(" c int");
-  allDatabaseRoot->next->rootTable->next->name = "b";
-  drop("drop hao b");
-  printf("%s ***\n", allDatabaseRoot->name);
-  if (argc > 1) {
-    if (memcpy(argv[1], "drop", strlen(argv[1])) == 0) {
-      //    drop("huzi");
-      printf("%p ***\n", allDatabaseRoot);
+    allDatabaseRoot->next = NULL;
+    if(!allDatabaseRoot) {
+	printf("all is empty\n");
     }
-    if (memcmp(argv[1], "createTable", strlen(argv[1])) == 0) {
-      //allDatabaseRoot->rootTable = createTable(" int a , float b , noe  ");
+    createDatabase("huzi");
+    allDatabaseRoot->next->rootTable = createTable("  a int  , b float  , noe  ");
+    allDatabaseRoot->next->rootTable->name = "a";
+    allDatabaseRoot->next->rootTable->next = createTable(" c int");
+    allDatabaseRoot->next->rootTable->next->name = "b";
+//    alter_parse("
+    if (argc > 1) {
+	if (memcpy(argv[1], "drop", strlen(argv[1])) == 0) {
+	    //    drop("huzi");
+	    printf("%p ***\n", allDatabaseRoot);
+	}
+	if (memcmp(argv[1], "createTable", strlen(argv[1])) == 0) {
+	    //allDatabaseRoot->rootTable = createTable(" int a , float b , noe  ");
+	}
     }
-  }
 }
