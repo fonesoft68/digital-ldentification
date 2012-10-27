f406f7b4e5d9a458fbb8244853d228cdf1f32ea6
huzihao <zihaohu@hustunique>
2012-10-27 16:37:09 +0800

    g

--- seekCup/Src/sql.h
+++ seekCup/Src/sql.h
@@ -126,7 +126,6 @@ int outputForOrder(char **, int *, int);
 int show_parse(char *);
 col *find(table * tb, char *);
 int resCmp(TYPE type, char *, char *);
-<<<<<<< HEAD
 item *itemcpy(item*);
 col *colcpy(col*);
 table *tablecpy(table*);
@@ -138,11 +137,6 @@ int isSelect(TYPE, char*, char*);
 int isNum(char *);
 int isFloat(char *);
 int isText(char *);
-=======
 void swap(table *, int i, int j);
-<<<<<<< HEAD
 void sort(table *, char *, int );
-=======
-table *sort(table *, char *, int );
->>>>>>> e99b89287f1febdd1c7a70b1fe38ff12632cb49d
->>>>>>> 5a52b156d1a9b5d5e7d3702b9ada667e2041a3b4
+
