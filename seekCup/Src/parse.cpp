d46660ddae828f77ac3742b325e4551fe27be7e1
atupal <kangleyu@hustunique.com>
2012-10-28 23:46:30 +0800

    merger

--- seekCup/Src/parse.cpp
+++ seekCup/Src/parse.cpp
@@ -206,7 +206,6 @@ int parseCommand(char * command)
 #ifdef DEBUG
 		printf("$select:%s$\n", command);
 #endif
-<<<<<<< HEAD
 		printf("***********************\n");
 		int *cc = (int *) calloc (1, sizeof(int));
 		char **split_tmp = split(command, "select * from", cc);
@@ -215,7 +214,6 @@ int parseCommand(char * command)
 		// 	return 0;
 		// }
 
-=======
 //		printf("***********************\n");
 //		int *cc = (int *) calloc (1, sizeof(int));
 //		char **split_tmp = split(command, "select * from", cc);
@@ -223,7 +221,6 @@ int parseCommand(char * command)
 //			showTableContext(findTable(split_tmp[0]));
 //			return 0;
 //		}
->>>>>>> 74cd0428f70d8b04338c66d5de1082cb62450207
 		select(command);
 	}
 	else if (result_updata[0] == 1&& result_create_database[1] == begin_black) {
